e7e28861bb326521be57c3ceb8d50fb425f272b2
SarraSoya <86721475+SarraSoya@users.noreply.github.com>
2021-07-16 10:01:12 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,56 @@
+//counter + and -
+var plus = document.getElementsByClassName('btn-plus');
+var counter = document.getElementsByClassName('counter');
+var minus = document.getElementsByClassName('btn-minus');
+
+for (let i = 0; i < plus.length; i++) {
+  plus[i].addEventListener('click', function () {
+    counter[i].innerText++;
+    Total();
+  });
+
+
+  minus[i].addEventListener('click', function () {
+    if (counter[i].innerText > 0) counter[i].innerText--;
+    Total();
+  });
+}
+
+// heart icon:from black to red
+var heartBtn=document.querySelectorAll('.heartBtn');
+var heart=document.querySelectorAll('.fa-heart');
+for (let i = 0; i < heartBtn.length; i++) {
+  heartBtn[i].addEventListener('click', function () {
+if (heart[i].style.color=='red') heart[i].style.color='black'
+else heart[i].style.color='red'
+
+  })}
+  //close icon:delete item
+var close=document.querySelectorAll('.close');
+var card=document.querySelectorAll('.card');
+for (let i = 0; i < close.length; i++) {
+  close[i].addEventListener('click', function () {
+    card[i].remove()
+    Total();
+})}
+
+//prix total
+function Total (){
+var counter = document.querySelectorAll('.counter');
+var prixTotal=document.querySelector('.prixTotal');
+var prix = document.querySelectorAll('.prix');
+var sum=0;
+for (let i = 0; i < counter.length; i++) {
+     sum+=counter[i].innerText*prix[i].innerText;
+}
+ prixTotal.innerText = sum;
+}
+
+
+     Total();
+    
+
+
+
+
+
